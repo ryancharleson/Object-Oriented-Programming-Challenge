d3157bc4919605c49f11a6b6ceeb614b3b71f70b
hrsprs <jamescharleson@yahoo.com>
2022-11-29 14:36:53 -0700

    initialized files and started on questions prompt

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+const prompts = [
+    {
+        type: 'list',
+        name: 'title',
+        message: 'What is the Title of your employee?',
+        choices: ['Intern', 'Engineer', 'Manager']
+
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your employee?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of your employee?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email of your worker?'
+    },
+    {
+        when: (answer) => answer.title === 'Engineer',
+        type: 'input',
+        name: 'github',
+        message: 'What is the Github username of your employee?'
+    },
+    {
+        when: (answer) => answer.title === 'Manager',
+        type: 'input',
+        name: 'officeNum',
+        message: 'What is the office number for your employee?'
+    },
+    {
+        when: (answer) => answer.title === 'Intern',
+        type: 'input',
+        name: 'school',
+        message: 'Where do they go to school?'
+    },
+];
+
